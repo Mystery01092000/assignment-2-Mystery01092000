f8a4199a5c9867ba8df26dacda94e419ff23e96f
Pranav Tomar <pranavtom1223@gmail.com>
2020-03-31 16:08:22 +0530

    Problem 3 compplete and working

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,24 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+
+    public static void main(String[] args)
+    {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("Pranav", 1);
+        obj.insert("rishabh",5);
+        obj.insert("kartik", 3);
+        obj.insert("sarvendra", 22);
+        obj.insert("naina",16);
+        obj.insert("Vrinda", 33);
+        System.out.println("Student according to their roll  number are: ");
+        obj.peep();
+
+    }
 }
