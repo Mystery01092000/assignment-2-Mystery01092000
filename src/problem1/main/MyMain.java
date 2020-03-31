548f5c487a757fdfc913281b8514713a20a2658c
Pranav Tomar <pranavtom1223@gmail.com>
2020-03-31 15:48:26 +0530

    problem 1 complete and running

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,26 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+
+    public static void main(String[] args)
+    {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(21);
+        obj.insert(22);
+        obj.insert(23);
+        obj.insert(30);
+        obj.insert(40);
+        obj.insert(50);
+        obj.insert(60);
+        obj.insert(70);
+        System.out.print("Left Child nodes are:  ");
+        obj.printLeftNode(obj.root);
+        System.out.println();
+        System.out.println("No of nodes that doesn't have a left child node are:  "+obj.getCount());
+        obj.preOrder(obj.root);
+    }
 }
+
--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -1,10 +1,105 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
+ *  User: Pranav Tomar
+ *  Assignment 2 Data structure and algorithms
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    public TreeNode root;
+    int count;
+
+    public MyBinarySearchTree()
+    {
+        root=null;
+    }
+
+    public void insert(int val)
+    {
+        TreeNode newnode = new TreeNode();
+        newnode.setData(val);
+        if(root==null)
+        {
+            root=newnode;
+        }
+        else
+        {
+            TreeNode current= root;
+            TreeNode parent;
+            while(true)
+            {
+                parent = current;
+                if(val < current.getData())
+                {
+                    parent.setLeft(newnode);
+                    return;
+                }
+                else
+                {
+                    current = current.getLeft();
+                    if(current==null)
+                    {
+                        parent.setRight(newnode);
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+    public void printLeftNode(TreeNode root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        else
+        {
+            if(root.getLeft() != null)
+                System.out.print(root.getLeft().getData());
+            else
+            {
+                count++;
+            }
+            printLeftNode(root.getLeft());
+            printLeftNode(root.getRight());
+        }
+    }
+    public int getCount()
+    {
+        return count;
+    }
+
+    public void preOrder(TreeNode root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        else
+        {
+            System.out.print(root.getData()+" ");
+            preOrder(root.getLeft());
+            preOrder(root.getRight());
+
+        }
+    }
+
+    public void postOrder(TreeNode root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        else
+        {
+            postOrder(root.getLeft());
+            postOrder(root.getRight());
+            System.out.print(root.getData()+" ");
+        }
+    }
+
 }
